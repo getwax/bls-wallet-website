29f75f76ec9aa8a0218cb1e204106d26023093cb
Blake Duncan <blakecduncan@gmail.com>
2023-09-06 12:26:22 -0400

    Remove unused variable

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 import Fade from 'react-reveal/Fade';
 import { useEffect, useState } from 'react';
 import { shuffled } from 'ethers/lib/utils';
